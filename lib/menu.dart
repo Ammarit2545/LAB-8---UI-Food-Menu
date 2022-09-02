75ee8a520d0eaaf3f2c0fee770da691cee949e91
Arm <armm4sonic@gmail.com>
2022-09-03 02:31:37 +0700

    1th - Create Form

---
+++ lib/menu.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  List<List<dynamic>> foodList = [
+    [
+      "https://image.shutterstock.com/image-illustration/gold-no1-emblem-260nw-340330580.jpg",
+      280
+    ],
+    ["https://i.ytimg.com/vi/1oBv9S8QV8k/maxresdefault.jpg", 300],
+    ["https://i.ytimg.com/vi/hN4EFQkgnAo/maxresdefault.jpg", 230],
+    ["https://i.ytimg.com/vi/1oBv9S8QV8k/maxresdefault.jpg", 290],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
